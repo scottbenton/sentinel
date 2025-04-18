5e74b84597703a5c5ae8d38091ec8bc3e97423ff
Scott Benton <scott@scottbenton.dev>
2025-04-17 21:08:56 -0400

    test(PWA): Reverting test text change

--- src/pages/auth/AuthPage.tsx
+++ src/pages/auth/AuthPage.tsx
@@ -36,10 +36,7 @@ export default function AuthPage() {
 
   return (
     <>
-      <PageHeader
-        title="Login or Create an Account Test Changes"
-        maxW="breakpoint-sm"
-      />
+      <PageHeader title="Login or Create an Account" maxW="breakpoint-sm" />
       <PageContent maxW="breakpoint-sm" p={4}>
         <Box display="flex" mb={4}>
           <Image src={"/SentinelWordmark.png"} alt="Sentinel" h={16} />
